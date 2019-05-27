357e728f51fcac88654f8ef4836c15c99ed33afc
Volodymyr Khytskyi <khytsky.vladimir@gmail.com>
2019-05-27 15:43:20 +0200

    day3 -- part 1 - error handling exercise

--- day3/src/lib.rs
+++ day3/src/lib.rs
@@ -1,30 +1,47 @@
-use std::num::ParseIntError;
+use std::fs::File;
+use std::io::{BufReader, BufRead, Error};
+use regex::Regex;
 
-struct Claim {
+#[derive(Debug, PartialEq)]
+pub struct Claim {
   startX: usize,
   startY: usize,
   endX: usize,
   endY: usize,
 }
 
-// fn parse(input: &String) -> Result<Claim, String> {
-fn parse(input: &String) -> Result<Vec<usize>, ParseIntError> {
-  // let [startX, startY, width, height] = input
-  input
-    .split("@ ")
+fn parse(input: &String) -> Result<Claim, String> {
+  let re = Regex::new(r".*@\s(\d+),(\d+):\s(\d+)x(\d+)")
+    .unwrap();
+
+  let caps = re
+    .captures(input)
+    .ok_or("could not match input")?;
+
+  let matches = caps
+    .iter()
     .skip(1)
-    .flat_map(|x| x.split(": "))
-    .flat_map(|x| x.split(|c| c == ',' || c == 'x'))
-    .map(|s| s.parse::<usize>())
-    .try_fold(Vec::new(), |mut acc, r| {
-      match r.clone().err() {
-        Some(err) => Err(err),
-        None => {
-          acc.push(r.unwrap());
-          Ok(acc)
-        }
-      }
-    })
+    .map(|o| o.unwrap())
+    .map(|m| m.as_str())
+    .map(|s| s.parse::<usize>().map_err(|e| e.to_string()))
+    .collect::<Result<Vec<usize>, _>>()?;
+
+  Ok(Claim { 
+    startX: matches[0], 
+    startY: matches[1], 
+    endX: matches[0] + matches[2],
+    endY: matches[1] + matches[3],
+  })
+}
+
+pub fn read_input(filepath: &str) -> Result<Vec<Claim>, String> {
+  let file = File::open(filepath).map_err(|e| e.to_string())?;
+  let claim_results = BufReader::new(file)
+    .lines()
+    .map(|line_r| parse(&line_r.map_err(|e| e.to_string())?))
+    .collect::<Result<Vec<Claim>, _>>()?;
+
+  Ok(claim_results)
 }
 
 #[cfg(test)]
@@ -32,23 +49,26 @@ mod tests {
   use super::*;
 
   #[test]
-  fn parse_test() {
-    // assert_eq!(parse(&String::from("#1 @ 1,1: 1x1")).is_ok(), true);
-
-    // assert_eq!(parse(&String::from("#1 @ 1,1: 1x")).is_ok(), false);
-    // assert_eq!(parse(&String::from("#1 @ 1,1: x1")).is_ok(), false);
-    // assert_eq!(parse(&String::from("#1 @ 1,: 1x1")).is_ok(), false);
-    // assert_eq!(parse(&String::from("#1 @ ,1: 1x1")).is_ok(), false);
-    // assert_eq!(parse(&String::from("#1 @ ,: 1x1")).is_ok(), false);
-    // assert_eq!(parse(&String::from("#1 @ ,: x")).is_ok(), false);
-    // assert_eq!(parse(&String::from("#1 @ a,b: -1x-2")).is_ok(), false);
+  fn parse_sucess_test() {
+    assert_eq!(
+      parse(&String::from("#1 @ 1,1: 1x1")).unwrap(), 
+      Claim { startX: 1, startY: 1, endX: 2, endY: 2}
+    );
+    
+    assert_eq!(parse(&String::from("#1 @ 1,1: 1x")).is_ok(), false);
+    assert_eq!(parse(&String::from("#1 @ 1,1: x1")).is_ok(), false);
+    assert_eq!(parse(&String::from("#1 @ 1,: 1x1")).is_ok(), false);
+    assert_eq!(parse(&String::from("#1 @ ,1: 1x1")).is_ok(), false);
+    assert_eq!(parse(&String::from("#1 @ ,: 1x1")).is_ok(), false);
+    assert_eq!(parse(&String::from("#1 @ ,: x")).is_ok(), false);
+    assert_eq!(parse(&String::from("#1 @ a,b: -1x-2")).is_ok(), false);
 
     assert_eq!(parse(&String::from("")).is_ok(), false);
-    // assert_eq!(parse(&String::from("#1 1,1: 1x1")).is_ok(), false);
-    // assert_eq!(parse(&String::from("#1 @ 1,1 1x1")).is_ok(), false);
-    // assert_eq!(parse(&String::from("#1 @ 1,1 @ 1x1")).is_ok(), false);
-    // assert_eq!(parse(&String::from("#1 : 1,1 : 1x1")).is_ok(), false);
-    // assert_eq!(parse(&String::from("#1 , 1,1 , 1x1")).is_ok(), false);
-    // assert_eq!(parse(&String::from("#1 , 1, , x1")).is_ok(), false);
+    assert_eq!(parse(&String::from("#1 1,1: 1x1")).is_ok(), false);
+    assert_eq!(parse(&String::from("#1 @ 1,1 1x1")).is_ok(), false);
+    assert_eq!(parse(&String::from("#1 @ 1,1 @ 1x1")).is_ok(), false);
+    assert_eq!(parse(&String::from("#1 : 1,1 : 1x1")).is_ok(), false);
+    assert_eq!(parse(&String::from("#1 , 1,1 , 1x1")).is_ok(), false);
+    assert_eq!(parse(&String::from("#1 , 1, , x1")).is_ok(), false);
   }
 }
---
+++ day3/src/main.rs
@@ -0,0 +1,8 @@
+use day3::read_input;
+
+fn main() {
+  let filepath = "./input.txt";
+  let input = read_input(&filepath);
+
+  println!("Hello world");
+}
