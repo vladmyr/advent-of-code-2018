a6fe5a988c4a650d061e45e6260e4c6a9840668f
Volodymyr Khytskyi <khytsky.vladimir@gmail.com>
2019-06-04 17:08:34 +0200

    day4 -- part 1 -- WIP

--- day4/src/lib.rs
+++ day4/src/lib.rs
@@ -1,6 +1,7 @@
-use std::cmp::Ordering;
+use std::cmp::{Ordering, max};
 use std::fs::File;
 use std::io::{BufReader, BufRead};
+use std::collections::HashMap;
 
 use regex::Regex;
 use chrono::prelude::*;
@@ -80,6 +81,38 @@ pub fn read_input(filepath: &str) -> Result<Vec<Record>, String> {
   Ok(claim_results)
 }
 
+fn filter_map_records(records: &Vec<Record>) -> HashMap<usize, HashMap<u8, usize>> {
+  records
+    .iter()
+    .fold((0, HashMap::new() as HashMap<usize, HashMap<u8, usize>>), |(id, mut map), r| {
+      let id = match r.action {
+        Action::Shift(new_id) => new_id,
+        Action::FallAsleep | Action::WakeUp => {
+          if r.datetime.time().hour() == 0 {
+            let target_minute = if r.action == Action::WakeUp {
+              max(r.datetime.time().minute() - 1, 0) as u8
+            } else {
+              r.datetime.time().minute() as u8
+            };
+
+            let count = map
+              .entry(id)
+              .or_insert(HashMap::new())
+              .entry(target_minute)
+              .or_insert(0);
+
+            *count += 1;
+          }
+
+          id
+        },
+      };
+
+      (id, map)
+    })
+    .1
+}
+
 #[cfg(test)]
 mod tests {
   use super::*;
@@ -114,5 +147,79 @@ mod tests {
     assert!(
       parse(&String::from("[1970-01-01 23:59] Guard #1 begins shift")).is_ok()
     );
+  }
+
+  #[test]
+  fn filter_map_records_test() {
+    let v = vec![
+      Record { 
+        datetime: Utc.ymd(1518, 11, 1).and_hms(0, 0, 0),
+        action: Action::Shift(10)
+      },
+      Record { 
+        datetime: Utc.ymd(1518, 11, 1).and_hms(0, 5, 0),
+        action: Action::FallAsleep
+      },
+      Record { 
+        datetime: Utc.ymd(1518, 11, 1).and_hms(0, 25, 0),
+        action: Action::WakeUp
+      },
+      Record { 
+        datetime: Utc.ymd(1518, 11, 1).and_hms(0, 30, 0),
+        action: Action::FallAsleep
+      },
+      Record { 
+        datetime: Utc.ymd(1518, 11, 1).and_hms(0, 55, 0),
+        action: Action::WakeUp
+      },
+      Record { 
+        datetime: Utc.ymd(1518, 11, 1).and_hms(23, 58, 0),
+        action: Action::Shift(99)
+      },
+      Record { 
+        datetime: Utc.ymd(1518, 11, 2).and_hms(0, 40, 0),
+        action: Action::FallAsleep
+      },
+      Record { 
+        datetime: Utc.ymd(1518, 11, 2).and_hms(0, 50, 0),
+        action: Action::WakeUp
+      },
+      Record { 
+        datetime: Utc.ymd(1518, 11, 3).and_hms(0, 5, 0),
+        action: Action::Shift(10)
+      },
+      Record { 
+        datetime: Utc.ymd(1518, 11, 3).and_hms(0, 24, 0),
+        action: Action::FallAsleep
+      },
+      Record { 
+        datetime: Utc.ymd(1518, 11, 3).and_hms(0, 29, 0),
+        action: Action::WakeUp
+      },
+      Record { 
+        datetime: Utc.ymd(1518, 11, 4).and_hms(0, 2, 0),
+        action: Action::Shift(99)
+      },
+      Record { 
+        datetime: Utc.ymd(1518, 11, 4).and_hms(0, 36, 0),
+        action: Action::FallAsleep
+      },
+      Record { 
+        datetime: Utc.ymd(1518, 11, 4).and_hms(0, 46, 0),
+        action: Action::WakeUp
+      },
+      Record { 
+        datetime: Utc.ymd(1518, 11, 5).and_hms(0, 3, 0),
+        action: Action::Shift(99)
+      },
+      Record { 
+        datetime: Utc.ymd(1518, 11, 5).and_hms(0, 45, 0),
+        action: Action::FallAsleep
+      },
+      Record { 
+        datetime: Utc.ymd(1518, 11, 5).and_hms(0, 55, 0),
+        action: Action::WakeUp
+      },
+    ];
   }
 }
