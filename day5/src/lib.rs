44cda40e98189b66ec7385c7880e3b264464aa26
Volodymyr Khytskyi <khytsky.vladimir@gmail.com>
2019-06-08 18:35:40 +0200

    day 5 -- part 1

--- day5/src/lib.rs
+++ day5/src/lib.rs
@@ -5,7 +5,7 @@ use std::collections::BinaryHeap;
 pub fn read_input(filepath: &str) -> Result<Vec<char>, String> {
   let mut file = File::open(filepath).map_err(|e| e.to_string())?;
   let mut contents = String::new();
-  file.read_to_string(&mut contents);
+  file.read_to_string(&mut contents).map_err(|e| e.to_string())?;
 
   Ok(contents.chars().collect())
 }
@@ -16,12 +16,22 @@ fn get_is_matching(a: &char, b: &char) -> bool {
 }
 
 pub fn calc_part1(input: &Vec<char>) -> usize {
-  let mut heap: BinaryHeap<usize> = BinaryHeap::new();
-  for (i, c) in input.iter().enumerate() {
-    
+  let mut heap: BinaryHeap<usize> = [0]
+    .into_iter()
+    .cloned()
+    .collect();
+
+  for (i, c) in input.iter().enumerate().skip(1) {
+    match heap.peek() {
+      None => heap.push(i),
+      Some(j) => match get_is_matching(c, &input[*j]) {
+        false => heap.push(i),
+        true => { heap.pop(); },
+      }
+    };
   }
 
-  0
+  heap.len()
 }
 
 #[cfg(test)]
@@ -36,5 +46,10 @@ mod tests {
     assert!(!get_is_matching(&'a', &'b'));
     assert!(!get_is_matching(&'A', &'b'));
     assert!(!get_is_matching(&'a', &'B'));
+  }
+
+  #[test]
+  fn calc_part1_test() {
+    assert_eq!(calc_part1(&String::from("dabAcCaCBAcCcaDA").chars().collect()), 10);
   }
 }
