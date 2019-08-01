bc7a13876d1396481b07fff6e88ec4ae2bf7b902
Volodymyr Khytskyi <khytsky.vladimir@gmail.com>
2019-08-01 07:22:30 +0200

    day 6 -- part 2 - cleanup

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -2,12 +2,12 @@ use day6::read_input;
 use day6::calc_part1;
 use day6::calc_part2;
 
-static WATERLINE: u64 = 10000;
+static WATERMARK: u64 = 10000;
 
 fn main() {
   let filepath = "./input.txt";
   let input = read_input(filepath).unwrap();
 
   println!("Day #6, part #1 {:?}", calc_part1(&input));
-  println!("Day #6, part #2 {:?}", calc_part2(&input, &WATERLINE));
+  println!("Day #6, part #2 {:?}", calc_part2(&input, &WATERMARK));
 }
